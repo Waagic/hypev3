d93a878d7ee039f771dc5a49a653bbe444c05d66
Lucas <lucas.marguiron@gmail.com>
2024-10-15 17:02:05 +0200

    Entité média

---
+++ src/Entity/Media.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MediaRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MediaRepository::class)]
+class Media
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $texte = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $media = null;
+
+    #[ORM\Column]
+    private ?bool $aime = null;
+
+    #[ORM\Column]
+    private ?bool $consomme = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): static
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getTexte(): ?string
+    {
+        return $this->texte;
+    }
+
+    public function setTexte(?string $texte): static
+    {
+        $this->texte = $texte;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getMedia(): ?string
+    {
+        return $this->media;
+    }
+
+    public function setMedia(?string $media): static
+    {
+        $this->media = $media;
+
+        return $this;
+    }
+
+    public function isAime�e(): ?bool
+    {
+        return $this->aime�e;
+    }
+
+    public function setAime�e(bool $aime�e): static
+    {
+        $this->aime�e = $aime�e;
+
+        return $this;
+    }
+
+    public function isConsomme(): ?bool
+    {
+        return $this->consomme;
+    }
+
+    public function setConsomme(bool $consomme): static
+    {
+        $this->consomme = $consomme;
+
+        return $this;
+    }
+}
